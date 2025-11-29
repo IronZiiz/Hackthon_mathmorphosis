fd6b6e4f72e0b49b9c4c8791d853de71c07dae11
Thiago Zilli <thiagozilli10@gmail.com>
2025-11-29 15:37:07 -0300

    feat: implementa métrica de total respondentes

--- services/AvaliacaoInstitucionalService.py
+++ services/AvaliacaoInstitucionalService.py
@@ -18,13 +18,12 @@ class AvaliacaoInstitucionalService(DataLoader):
 
     def total_respondentes_ano_atual(self): 
         df = self.df_load_dados_institucional
-        df 
-        qtd_respondentes_atual = 2000
+        qtd_respondentes_atual = len(df['ID_PERGUNTA'].unique())
         return qtd_respondentes_atual
     
     def total_respondentes_ano_passado(self): 
         qtd_respondentes_ano_atual = self.total_respondentes_ano_atual()
-        qtd_respondentes_ano_passado = 1000
+        qtd_respondentes_ano_passado = 50
         pct_comparacao_ano_atual =(qtd_respondentes_ano_atual/qtd_respondentes_ano_passado - 1)*100
         return pct_comparacao_ano_atual,qtd_respondentes_ano_passado
     
